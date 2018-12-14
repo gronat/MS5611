31e5b1c4d5f62fd2c6ed75ba1ebb9890141d1230
red-kite <red-kite@milanhermann.de>
2018-12-14 09:10:32 +0100

    Structure code and introduce functions
    New functions:
    * triggerRawPressure
    * readRawPressure
    * triggerRawTemperature
    * readRawTemperature
    * calculateTemperature
    * calculatePressure
    
    Old functions are now using the above and should work as before
    * getRawPressure
    * getRawTemperature
    * getPressure
    
    Goal is to trigger sensor readings from within other code
    
    ----
    
    Add example function tutorial
    
    Add describing names for variables
    (_T -> _temp, _P -> _pressure, ...)
    
    ----
    
    Add function init as alternative for begin without initialising Wire

--- MS5611.h
+++ MS5611.h
@@ -17,16 +17,28 @@ by Petr Gronat@2014
 
 
 // address of the device MS5611
-#define ADD_MS5611 0x77 	// can be 0x76 if CSB pin is connected to GND
+#define ADD_MS5611 0x77
+// 0x77 if CSB pin is connected to GND
+// 0x76 if CSB pin is connected to VCC
 
 class MS5611{
 	public:
 		MS5611();		//constructor
 			void 		begin();
+			void            init();
+			// Read digital pressure and temperature data
+			void        triggerRawTemperature();
+			uint32_t    readRawTemperature();
 			uint32_t 	getRawTemperature();
-			int32_t 	getTemperature();
+			void        triggerRawPressure();
+			uint32_t    readRawPressure();
 			uint32_t 	getRawPressure();
+			// Calculate Temperature
+			int32_t     calculateTemperature(uint32_t D2);
+			int32_t     calculatePressure(uint32_t D1);
+			int32_t 	getTemperature();
 			int32_t 	getPressure();
+			int32_t     tutorial();
 			void 		readCalibration();
 			void 		getCalibration(uint16_t *);
 			void 		sendCommand(uint8_t);
@@ -34,10 +46,10 @@ class MS5611{
 	private:
 			void 		reset();
 		//variables
-		int32_t 	_P;
-		int32_t  	_T;
+		int32_t 	_pressure;
+		int32_t  	_temp;
 		int32_t 	_dT;
-		uint16_t 	_C[N_PROM_PARAMS];
+		uint16_t 	_calib[N_PROM_PARAMS];
 		uint32_t 	_lastTime;
 };
 
